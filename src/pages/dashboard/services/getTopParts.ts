080d041cc190a05f2d1fe332dc6ac4de7da0d9f0
iaifrns <nsinifranc@gmail.com>
2026-06-23 19:31:08 +0100

    finished the modification

--- src/pages/dashboard/services/getTopParts.ts
+++ src/pages/dashboard/services/getTopParts.ts
@@ -2,9 +2,9 @@ import { getMostUsedPartUrl } from "../../../constants/endpoints"
 
 export const getTopParts = async () => {
     try{
+        console.log(getMostUsedPartUrl)
         const response = await fetch(getMostUsedPartUrl)
         const result = await response.json()
-        console.log(getMostUsedPartUrl)
         if(result.success){
             return [result.data[0].topParts,result.data[0].grandTotal[0].totalParts]
         }
